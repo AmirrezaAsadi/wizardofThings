8ea54ca10ba7ddd5f84dd92dc18d35b2af2b5bea
amir <5337255+AmirrezaAsadi@users.noreply.github.com>
2024-01-30 21:35:21 -0500

    Initial Commit

---
+++ WizardofHome/WizardofHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WizardofHomeApp.swift
+//  WizardofHome
+//
+//  Created by Amir on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct WizardofHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
